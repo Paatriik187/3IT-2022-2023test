bc5307716a7310f9a7b55f0d809f6fd67b41bb01
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2022-09-14 11:10:49 +0200

    .

---
+++ src/main/java/sk/sosholic/prvypolrok/DatoveTypy.java
@@ -0,0 +1,17 @@
+package sk.sosholic.prvypolrok;
+
+public class DatoveTypy {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tu su primitivne datove typy:");
+        System.out.println("int uchovava prirodzene cislo velkosti 32b");
+        System.out.println("boolean uchovava hodnotu true alebo false");
+        System.out.println("byte uchovava cislo prirodzene velkosti 8b");
+        System.out.println("char uchovava character");
+        System.out.println("short uchovava cislo prirodzene velkosti 16b");
+        System.out.println("long uchovava prirodzene cislo velkosti 64b ");
+        System.out.println("float uchovava realne cislo velkosti 32b");
+        System.out.println("double uchovava realne cislo velkosti 64b");
+    }
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/Opakovanie.java
@@ -0,0 +1,10 @@
+package sk.sosholic.prvypolrok;
+
+public class Opakovanie {
+
+    public static void main(String[] args){
+        System.out.println("Hledal sem prax v skalici ked prsalo a ja sem nemnel dazdnik, tak sem zmokel");
+        System.out.println("Nedostal sem penize na narodeniny od tatu, ani sem ho nevidel cele prazdniny");
+
+    }
+}
